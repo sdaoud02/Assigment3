bbdd0002b48d7972bd78b2bf37446e050716b6ca
lmanta17 <lmant900@mtroyal.ca>
2025-11-26 21:21:36 -0700

    Add files via upload

---
+++ SampleController.java
@@ -0,0 +1,52 @@
+package mru.tsc.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Tab;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class SampleController {
+
+    @FXML
+    private Tab homeTab;
+
+    @FXML
+    private AnchorPane listView;
+
+    @FXML
+    private RadioButton nmButton;
+
+    @FXML
+    private RadioButton snButton;
+
+    @FXML
+    private RadioButton tyButton;
+
+    @FXML
+    void homeSelect(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nmbtnHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void show(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void snbtnHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tybtnHandler(ActionEvent event) {
+
+    }
+
+}
